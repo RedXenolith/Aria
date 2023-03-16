5ae262657f13ce08ab9524bfc12b5100a615d3ea
Diego Guzmán <saes231@gmail.com>
2023-03-16 04:30:46 -0500

    Campos de texto de nombre y correo fueron añadidos

--- Aria/ContentView.swift
+++ Aria/ContentView.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name:String = ""
+    @State var email:String = ""
     var body: some View {
         VStack {
-            Image(systemName: "creditcard.and.123")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Aria")
                 .font(.largeTitle)
+                .padding(.bottom, 8.0)
+            Image(systemName: "creditcard.and.123")
+                .imageScale(.large)
+                .foregroundColor(.purple)
+                .padding(.bottom, 6.0)
+            TextField("Introduce tu nombre", text: $name)
+                .padding()
+            TextField("Introduce tu coreo", text: $email)
+                .padding()
+            
         }
         .padding()
-        .frame(width: 300, height: 100, alignment: .top)
+        .frame(width: 300, height: 400, alignment: .top)
     }
 }
 
