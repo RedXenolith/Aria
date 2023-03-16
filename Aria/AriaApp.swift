5601d12083d28d8fe0b24fe94653387eff149b17
Diego Guzmán <saes231@gmail.com>
2023-03-16 02:49:53 -0500

    Initial Commit

---
+++ Aria/AriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AriaApp.swift
+//  Aria
+//
+//  Created by Diego Guzmán on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
